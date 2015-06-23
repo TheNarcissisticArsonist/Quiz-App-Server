9c5f1ebe7f9864ee3967d7d407a3f8aaf4f7e100
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-23 11:23:30 -0400

    Get request info.

---
+++ Main.php
@@ -0,0 +1,10 @@
+<?php
+$line       = $_REQUEST["QuestionSet"];
+$pic        = $_REQUEST["Pictures"];
+$question   = $_REQUEST["Questions"];
+$a          = $_REQUEST["A"];
+$b          = $_REQUEST["B"];
+$c          = $_REQUEST["C"];
+$d          = $_REQUEST["D"];
+$answer     = $_REQUEST["Answers"];
+?>
