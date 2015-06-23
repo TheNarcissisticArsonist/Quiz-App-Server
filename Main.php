65a4f8b1f609bb3fe44c51b5c20d4a793129c8d8
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-23 14:50:30 -0400

    Fixed exlpode() not working.

--- Main.php
+++ Main.php
@@ -2,7 +2,7 @@
 $line = $_REQUEST["QuestionSet"];
 $file = fopen("QuestionData.csv", "r");
 $wholeFile = fread($file, filesize("QuestionData.csv"));
-$separatedFile = explode("\n", $wholeFile);
+$separatedFile = explode(PHP_EOL, $wholeFile);
 echo($separatedFile[$line-1]);
 fclose($file);
 ?>
