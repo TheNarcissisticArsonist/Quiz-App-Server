b06c94d20b44c4a487b3e69e7971ff50da5cb28a
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-24 11:31:25 -0400

    Added users

---
+++ Login.php
@@ -0,0 +1,23 @@
+<?php
+$user = $_REQUEST["user"];
+$pass = $_REQUEST["pass"];
+$file = fopen("UserList.csv", "r");
+$wholeFile = fread($file, filesize("UserList.csv"));
+$separatedFile = explode(PHP_EOL, $wholeFile);
+$userExists = false;
+for($i=0; $i<count($separatedFile); $i++) {
+  $separatedLine = explode(",", $separatedFile[$i]);
+  $compare1 = $separatedLine[0].",".$separatedLine[1];
+  $compare2 = $user.",".$pass;
+  if($compare1 == $compare2) {
+    $userExists = true;
+    break;
+  }
+}
+if($userExists) {
+  echo("OK,".$separatedLine[2]);
+}
+else {
+  echo("WRONG PASSWORD");
+}
+?>
