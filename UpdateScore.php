c638f797893fe4320a8460000ff9506e80fc68c4
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-24 13:01:32 -0400

    Can now update score

---
+++ UpdateScore.php
@@ -0,0 +1,19 @@
+<?php
+$user     = $_REQUEST["user"];
+$newScore = $_REQUEST["score"];
+$scoreFile = fopen("UserList.csv", "r");
+$wholeFile = fread($scoreFile, filesize("UserList.csv"));
+fclose($scoreFile);
+$separatedByLines = explode(PHP_EOL, $wholeFile);
+$lines = [];
+$scoreFile = fopen("UserList.csv", "w");
+for($i=0; $i<count($separatedByLines); $i++) {
+  if($user == explode(",", $separatedByLines[$i])[0]) {
+    $newLine = explode(",", $separatedByLines[$i])[0].",".explode(",", $separatedByLines[$i])[1].",".((string)$newScore);
+    $separatedByLines[$i] = $newLine;
+    fwrite($scoreFile, $separatedByLines[$i]);
+    fwrite($scoreFile, PHP_EOL);
+    break;
+  }
+}
+?>
